565c2c939dc84fa035bb7714b6a701c095f8a8bf
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-08 17:13:27 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,11 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var randomNumber=function(min,max){
+    var value = Math.floor (Math.random()*(max-min+1)+min);
+    return value;
+};
+
 var fight = function(enemyName) {
     //window.alert("Welcome to Robot Gladiators!");
     while (enemyHealth>0 && playerHealth >0){
@@ -18,14 +23,14 @@ var fight = function(enemyName) {
 
         if (confirmSkip){
             window.alert(playerName + " has chosen to skip the fight. Goodbye!");
-            playerMoney = playerMoney -10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         }
         
     }
-    
-        enemyHealth = enemyHealth - playerAttack;
+    var damage= randomNumber(playerAttack-3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - playerAttack);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -37,8 +42,9 @@ var fight = function(enemyName) {
         } else{
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
-
-        playerHealth = playerHealth-enemyAttack;
+        
+        var damage=randomNumber(enemyAttack-3,enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log (
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -60,7 +66,8 @@ var startGame = function (){
       if(playerHealth>0){
           window.alert("Welcome to Robot Gladiators! Round " + (i+1));
           var pickedEnemyName = enemyNames[i];
-          enemyHealth = 50;
+          var damage = randomNumber(playerAttack-3,playerAttack);
+          enemyHealth=Math.max(0,enemyHealth-damage);
           fight(pickedEnemyName);
 
           if(i<enemyNames.length-1 && playerHealth >0){
