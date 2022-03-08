f539b85575c660f5b98187901bcda9fce6a24138
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-07 20:54:20 -0500

    one round of fight has been set

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,36 @@
-window.prompt("What is your robot's name?");
+var playerName = window.prompt ("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack =10;
 
-var playerName = window.prompt("What is your robot's name?");
-//Note the lack of quotation marks around playName 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
 
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight(){
-    window.alert ("The fight has begun!");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    } else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth-enemyAttack;
+    console.log (
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert (playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-//fight();
+fight();
